5bcefa7381312bfecb792cf3629768e3a9c84239
Nadeen Gamage <nadeengamage@gmail.com>
2020-07-18 20:05:31 +0530

    Updated the Readme file

--- handshaker.sh
+++ handshaker.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
-cd ./build
-java -jar handshaker.jar
+execute_file() {
+	echo "Srat the process!"
+	java -jar handshaker.jar
+}
+
+if nc -zw1 github.com 443 && echo |openssl s_client -connect github.com:443 2>&1 |awk '
+  handshake && $1 == "Verification" { if ($2=="OK") exit; exit 1 }
+  $1 $2 == "SSLhandshake" { handshake = 1 }'
+then
+	if [ -e handshaker.jar ]
+	then
+	    execute_file
+	else
+	    curl https://github.com/nadeengamage/handshaker/blob/master/build/handshaker.jar -o handshaker.jar
+	    execute_file
+	fi
+fi
